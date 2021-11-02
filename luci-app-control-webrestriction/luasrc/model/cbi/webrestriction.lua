a5c9430cdbfcca5a5a9635617f706855dc564516
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-02 16:32:29 +0000

    luci-app-control-webrestriction: tidy up lua

--- luci-app-control-webrestriction/luasrc/model/cbi/webrestriction.lua
+++ luci-app-control-webrestriction/luasrc/model/cbi/webrestriction.lua
@@ -1,7 +1,10 @@
 local o = require "luci.sys"
 local a, e, t
 
-a = Map("webrestriction", translate("访问限制"), translate("使用黑名单或者白名单模式控制列表中的客户端是否能够连接到互联网。"))
+a = Map("webrestriction")
+a.title = translate("访问限制")
+a.description = translate("使用黑名单或者白名单模式控制列表中的客户端是否能够连接到互联网。")
+
 a:section(SimpleSection).template = "webrestriction/webrestriction_status"
 
 e = a:section(TypedSection, "basic")
@@ -16,7 +19,9 @@ t:value("whitelist", translate("白名单"))
 t:value("blacklist", translate("黑名单"))
 t.rmempty = false
 
-e = a:section(TypedSection, "macbind", translate("名单设置"), translate("如果是黑名单模式，列表中的客户端将被禁止连接到互联网；白名单模式表示仅有列表中的客户端可以连接到互联网。"))
+e = a:section(TypedSection, "macbind")
+e.title = translate("名单设置")
+e.description = translate("如果是黑名单模式，列表中的客户端将被禁止连接到互联网；白名单模式表示仅有列表中的客户端可以连接到互联网。")
 e.template = "cbi/tblsection"
 e.anonymous = true
 e.addremove = true
@@ -28,4 +33,5 @@ t = e:option(Value, "macaddr", translate("MAC地址"))
 t.rmempty = true
 
 o.net.mac_hints(function(e, a) t:value(e, "%s (%s)" % {e, a}) end)
+
 return a
