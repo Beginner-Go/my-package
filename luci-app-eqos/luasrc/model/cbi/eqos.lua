50e1d614b9e8d1c55a001789fb78cfbb769d0326
Beginner-Go <70857188+Beginner-Go@users.noreply.github.com>
2021-11-02 16:18:40 +0000

    luci-app-eqos: tidy up lua

--- luci-app-eqos/luasrc/model/cbi/eqos.lua
+++ luci-app-eqos/luasrc/model/cbi/eqos.lua
@@ -1,27 +1,32 @@
 local ipc = require "luci.ip"
 
-local m = Map("eqos", translate("Network speed control service"),translate("Limit client speed"))
-m:section(SimpleSection).template  = "eqos/eqos_status"
+m = Map("eqos")
+m.title = translate("Network speed control service")
+m.description = translate("Limit client speed")
 
-local s = m:section(TypedSection, "eqos", "")
+m:section(SimpleSection).template = "eqos/eqos_status"
+
+s = m:section(TypedSection, "eqos")
 s.anonymous = true
 
-local e = s:option(Flag, "enabled", translate("Enable"))
+e = s:option(Flag, "enabled", translate("Enable"))
 e.rmempty = false
 
-local dl = s:option(Value, "download", translate("Download speed (Mbit/s)"), translate("Total bandwidth"))
+dl = s:option(Value, "download", translate("Download speed (Mbit/s)"))
+dl.description = translate("Total bandwidth")
 dl.datatype = "and(uinteger,min(1))"
 
-local ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"), translate("Total bandwidth"))
+ul = s:option(Value, "upload", translate("Upload speed (Mbit/s)"))
+ul.description = translate("Total bandwidth")
 ul.datatype = "and(uinteger,min(1))"
 
 s = m:section(TypedSection, "device", translate("Speed limit based on IP address"))
 s.template = "cbi/tblsection"
 s.anonymous = true
 s.addremove = true
-s.sortable  = true
+s.sortable = true
 
-local ip = s:option(Value, "ip", translate("IP address"))
+ip = s:option(Value, "ip", translate("IP address"))
 
 ipc.neighbors({family = 4, dev = "br-lan"}, function(n)
 	if n.mac and n.dest then
